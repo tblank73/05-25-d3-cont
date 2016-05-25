18699fcd6e0a60b27598dc51020a922835b152d2
Tim Blankemeyer <tblank73@uw.edu>
2016-05-25 10:22:36 -0700

    Add D3 class code

--- script.js
+++ script.js
@@ -7,23 +7,69 @@ var dataList = [
    {id:3, name:'C', sleep:6},
 ];
 
-var svg = d3.select('svg');
+function update(dataList){
+  var widthScale = d3.scale.linear() //create the linear width scaling (domain to range)
+         .domain([0, 24])   //sleep interval
+         .range([10, 390]); //pixel interval
+         
+  var sleepMax = d3.max(dataList, function(d){
+    return d.sleep;
+  });
+  
+  var xAxis = d3.svg.axis().scale(widthScale)
+  
+  svg.append('g') //to position the axis
+  
+  var svg = d3.select('svg'); //creating var to refer to svg element
+  
+  var rectList = svg.selectAll('rect'); //go to svg element in DOM, and select all rect elements in svg block
+  
+  var dataJoin = rectList.data(dataList, function(item){
+    return item.id;
+  }); //bind the dataList entries with the rect elements
+  
+  dataJoin.enter().append('rect') //using .enter() to append rects for all datajoin items w/o rects
+  
+  dataJoin //if you call the attr on a collection, such as this array, D3 reads that as a "forEach"
+    .on('click', function(item){ //make it 1 longer on a click
+      console.log('clicky clicky')
+      item.sleep += 1;
+      update(dataList); //call the update function to update the dataset accordingly
+    })
+    .on('mouseover', function(item){
+      d3.select(this).attr('fill', 'yellow') //make it yellow on mouseover
+    })
+    .on('mouseout', function(item){ //go back to the if-else conditions on mouseout
+      d3.select(this).attr('fill', function(item){
+        if(item.sleep <= 4){
+        return 'red';
+      }
+      else{
+        return 'blue';
+      }
+      })
+    })
+    .transition()
+    .attr('fill', function(item){ //use conditional function to determine which fills are red/blue
+      if(item.sleep <= 4){
+        return 'red';
+      }
+      else{
+        return 'blue';
+      }
+    })
+    .attr({x:10, height:40}) //all rects have same x position and height
+    .attr('y', function(dataItem){ //take the dataitem with key id and multiply by 50 to get y pos
+      return dataItem.id*50;
+    })
+    .attr('width', function(item){ //take the item with key id and multiply by 20 to get width
+      return widthScale(item.sleep);
+    }); // note this is the only semicolon to denote you're done modifying data rects
+  
+  dataJoin.exit().remove() //uses exit() to isolate list items with DOM but not data, then removing
+};
 
-var rectList = svg.selectAll('rect');
-
-var dataJoin = rectList.data(dataList);
-
-dataJoin
-	.attr({x:10, height:40})
-	.attr('y', function(dataItem){
-		return dataItem.id*50;
-	})
-	.attr('width', function(item){ 
-		return item.sleep*20;
-	}); 
-
-
-
+update(dataList);
 
 
 
@@ -45,23 +91,27 @@ $('#addButton').click(function(){
     sleep: Math.floor(Math.random()*24) //random sleep (integer)
   })
   console.log(dataList);
+  update(dataList);
 });
 
 $('#remButton').click(function(){
   //remove first person
   dataList.shift();
   console.log(dataList);
+  update(dataList);
 });
 
 $('#moreButton').click(function(){
   dataList[0].sleep += 1; //increase first guy
   console.log(dataList);
+  update(dataList);
 });
 
 $('#lessButton').click(function(){
   //decrease first guy; min 0
   dataList[0].sleep = Math.max(dataList[0].sleep - 1, 0);
   console.log(dataList);
+  update(dataList);
 });
 
 $('#resetButton').click(function(){
@@ -71,4 +121,5 @@ $('#resetButton').click(function(){
     {id:3, name:'C', sleep:6},
   ];
   console.log(dataList);
+  update(dataList);
 });
